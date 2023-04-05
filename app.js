bc94cca5d4e0d07b54c5278a391cc269ac68ee47
cjsanc1 <cjsanc1@gmail.com>
2023-04-05 17:44:46 -0600

    Christopher Portfolio PM Commit 4/5

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("hello world")
+// const kickImage = document.getElementById('kickImage')
+// const snareImage = document.getElementById('snareImage')
+// const hatImage = document.getElementById('hatImage')
+// const cymbalImage = document.getElementById('cymbalImage')
+
+function playAudio(audioFile) {
+    new Audio(audioFile).play();
+}
+
+document.addEventListener("keydown", function (evt) {
+    if (evt.code === "KeyS") {
+        playAudio("./drum-samples/Snare.mp3")
+    }
+    if (evt.code === "KeyA") {
+        playAudio("./drum-samples/kick.mp3")
+    }
+    if (evt.code === "KeyE") {
+        playAudio("./drum-samples/Hi-Hat.mp3")
+    }
+    if (evt.code === "KeyR") {
+        playAudio("./drum-samples/Cymbal.mp3")
+    }
+});
