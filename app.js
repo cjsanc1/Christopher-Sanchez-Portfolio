7c3852a4ca4c81a94ea75d3e3ad6b0118d310c63
cjsanc1 <cjsanc1@gmail.com>
2023-04-06 12:28:24 -0600

    Christopher AM Commit 4/6

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-// const kickImage = document.getElementById('kickImage')
-// const snareImage = document.getElementById('snareImage')
-// const hatImage = document.getElementById('hatImage')
-// const cymbalImage = document.getElementById('cymbalImage')
 
+// function to play audio, parameter comes from "onclick" notated in index
 function playAudio(audioFile) {
+// playing the audio
     new Audio(audioFile).play();
+    // using JS to add class to body element
     document.body.classList.add("change-cursor")
+    // setting a timer to remove said class from element
     setTimeout(() => {
         document.body.classList.remove("change-cursor");
       }, 1000);
 }
 
+// create event listeners for keys on computer keyboard
 document.addEventListener("keydown", function (evt) {
     if (evt.code === "KeyS") {
         playAudio("./drum-samples/Snare.mp3")
